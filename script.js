15d48638d39046a8a47f142bde40f80c00c05105
Pelicie <mecagoenlashormigas@gmail.com>
2024-12-26 15:40:13 -0500

    Terminando la logica de la pagina

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-// import { Fireworks } from "fireworks-js";
+const counter = document.querySelector(".counter");
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const minuts = document.getElementById("minuts");
+const seconds = document.getElementById("seconds");
 
-// const container = document.querySelector(".fireworks");
-// const fireworks = new Fireworks(container, {
-//     gravity: 9.5,
-// });
-// fireworks.start();
+setInterval(() => {
+  const lastDate = new Date(2025, 0, 1);
+  const actualDate = new Date();
+  const diferencia = lastDate - actualDate;
+
+  const time = {
+    days: Math.floor(diferencia / (1000 * 60 * 60 * 24)),
+    hours: Math.floor((diferencia / (1000 * 60 * 60)) % 24),
+    minuts: Math.floor((diferencia / (1000 * 60)) % 60),
+    seconds: Math.floor((diferencia / 1000) % 60),
+  };
+
+  days.textContent = time.days;
+  hours.textContent = time.hours;
+  minuts.textContent = time.minuts;
+  seconds.textContent = time.seconds;
+}, 1000);
