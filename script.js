49f252745cfd2e421482c0e5d025bc1384cf56ef
Pelicie <mecagoenlashormigas@gmail.com>
2024-12-27 18:49:52 -0500

    Cambiando el setInterval por un requestAnimationFrame

--- script.js
+++ script.js
@@ -4,7 +4,27 @@ const hours = document.getElementById("hours");
 const minuts = document.getElementById("minuts");
 const seconds = document.getElementById("seconds");
 
-setInterval(() => {
+//!Forma vieja de renderizacion
+// setInterval(() => {
+//   const lastDate = new Date(2025, 0, 1);
+//   const actualDate = new Date();
+//   const diferencia = lastDate - actualDate;
+
+//   const time = {
+//     days: Math.floor(diferencia / (1000 * 60 * 60 * 24)),
+//     hours: Math.floor((diferencia / (1000 * 60 * 60)) % 24),
+//     minuts: Math.floor((diferencia / (1000 * 60)) % 60),
+//     seconds: Math.floor((diferencia / 1000) % 60),
+//   };
+
+//   days.textContent = time.days;
+//   hours.textContent = time.hours;
+//   minuts.textContent = time.minuts;
+//   seconds.textContent = time.seconds;
+// }, 1000);
+
+//?Nueva y mejor forma con requestAnimatonFrame
+const updateTime = () => {
   const lastDate = new Date(2025, 0, 1);
   const actualDate = new Date();
   const diferencia = lastDate - actualDate;
@@ -20,4 +40,8 @@ setInterval(() => {
   hours.textContent = time.hours;
   minuts.textContent = time.minuts;
   seconds.textContent = time.seconds;
-}, 1000);
+
+  requestAnimationFrame(updateTime);
+};
+
+updateTime();
