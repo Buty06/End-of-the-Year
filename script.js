4c1393ea18de53aa8d97c52beafe8a475fe29a76
Pelicie <mecagoenlashormigas@gmail.com>
2024-12-30 14:56:05 -0500

    Agregando la musica del final

--- script.js
+++ script.js
@@ -4,24 +4,12 @@ const hours = document.getElementById("hours");
 const minuts = document.getElementById("minuts");
 const seconds = document.getElementById("seconds");
 
-//!Forma vieja de renderizacion
-// setInterval(() => {
-//   const lastDate = new Date(2025, 0, 1);
-//   const actualDate = new Date();
-//   const diferencia = lastDate - actualDate;
-
-//   const time = {
-//     days: Math.floor(diferencia / (1000 * 60 * 60 * 24)),
-//     hours: Math.floor((diferencia / (1000 * 60 * 60)) % 24),
-//     minuts: Math.floor((diferencia / (1000 * 60)) % 60),
-//     seconds: Math.floor((diferencia / 1000) % 60),
-//   };
-
-//   days.textContent = time.days;
-//   hours.textContent = time.hours;
-//   minuts.textContent = time.minuts;
-//   seconds.textContent = time.seconds;
-// }, 1000);
+//?Para la musica de la pagina en el momento exacto
+const sound = new Howl({
+  src: ["assets/musica.mp3"],
+});
+const musicDate = new Date(2024, 11, 30, 15, 0, 0, 0);
+let validator;
 
 //?Nueva y mejor forma con requestAnimatonFrame
 const updateTime = () => {
@@ -33,7 +21,7 @@ const updateTime = () => {
     days: Math.floor(diferencia / (1000 * 60 * 60 * 24)),
     hours: Math.floor((diferencia / (1000 * 60 * 60)) % 24),
     minuts: Math.floor((diferencia / (1000 * 60)) % 60),
-    seconds: Math.floor((diferencia / 1000) % 60),
+    seconds: Math.floor((diferencia / 1000) % 60)
   };
 
   days.textContent = time.days;
@@ -41,7 +29,17 @@ const updateTime = () => {
   minuts.textContent = time.minuts;
   seconds.textContent = time.seconds;
 
+  if (actualDate > musicDate && validator !== false) {
+    validator = true;
+
+    if (validator) {
+      sound.play();
+      validator = false;
+    }
+  }
+
   requestAnimationFrame(updateTime);
 };
 
+//?Ejecuta la funcion recursiva
 updateTime();
